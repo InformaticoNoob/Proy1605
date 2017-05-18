eb25bcf8c942a8c6bdead76bcd8621fb0a5fc5f7
cetecom <cetecom@LC08L1210>
2017-05-18 13:04:21 -0400

    Cerrar Sesion agregado

--- index.php
+++ index.php
@@ -3,23 +3,27 @@
 session_start();
 $_SESSION["saludo"]="Hola Mundo";
 
-
-
 ?>
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
+        <div>
+            <?php 
+                if(isset($_SESSION['USR']))
+                    {  
+            ?>
+            <a href="cerrar.php">Cerrar Sesion</a>
+        </div>
+        <?php } ?>
+       
+        
         <a href="revision.php"> Revisi&oacute;n sesi&oacute;n</a>
-        <?php if (isset($_SESSION['USR'])){?>
+        <?php if(!isset($_SESSION['USR'])){?>
         <form action="revision.php" method="post">
             <div><label>Usuario</label><input type="text" name="nombre"></div>
             <div><label>Clave</label><input type="password" name="clave"></div>
